3d0cf0bca27511d212c38fdda6e0b6a6d343f4e4
sunyang-629 <sunyang9876@hotmail.com>
2024-02-15 21:33:47 +1000

    1-install and set configuration for material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import './App.css'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
+import { CssBaseline } from '@mui/material'
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Transmax Dashboard</h1>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="App">
+        <header className="App-header">
+          <Typography variant="h2">Transmax Dashboard</Typography>
+        </header>
+      </div>
+    </ThemeProvider>
   )
 }
 
